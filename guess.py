bd4158233603a3c3593d82a706f74754256e2340
Shuk Kuen Wong <shukkuenwong@Shukde-MacBook-Air.local>
2018-02-12 11:05:47 +0800

    Made some changes to the guess.py

--- guess.py
+++ guess.py
@@ -2,9 +2,12 @@ from random import randint
 
 def main():
   print "Welcome to the number guessing game!"
+  print "You have 5 chances to guess! Good luck!"
   number = randint(1,10)
+  count = 0
   while True:
     guess = raw_input("Guess a number between one and ten: ")
+    count = count + 1
     if int(guess) == number:
       print "That's right!"
       break
@@ -12,7 +15,9 @@ def main():
       print "Sorry, that's too high.  Guess again please!"
     elif int(guess) < number:
       print "Sorry, that's too low.  Guess again please!"
-      
+    if count = 5:
+    	"Sorry! Five trials done!"
+    	break  
 if __name__ == '__main__':
   main()
   
